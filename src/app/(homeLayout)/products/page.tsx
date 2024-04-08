cbab8d8a35b0adfa2725ac2913ef0ddb84076faf
Sujoy-Kumar-Das <sujoykumardas75@gmial.com>
2024-04-08 17:09:53 +0600

    filter added

--- src/app/(homeLayout)/products/page.tsx
+++ src/app/(homeLayout)/products/page.tsx
@@ -1,22 +1,65 @@
+"use client";
 import AllProductsGrid from "@/components/all-products/AllProductsGrid";
 import Container from "@/components/shared/Container";
 import Header from "@/components/shared/Header";
+import { TProductItem } from "@/types";
+import { ChangeEvent, useEffect, useState } from "react";
 
-export default async function Products() {
-  const res = await fetch("http://localhost:5000/products", {
-    next: {
-      revalidate: 30,
-    },
-  });
-  const data = await res.json();
+export default function Products() {
+  const [data, setData] = useState<TProductItem[]>([]);
+  const [filteredData, setFilteredData] = useState<TProductItem[]>([]);
+
+  const fetchData = async () => {
+    try {
+      const res = await fetch("http://localhost:5000/products");
+      const data = await res.json();
+      setData(data.data);
+      setFilteredData(data.data);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const handleFilterChange = (event: ChangeEvent<HTMLSelectElement>) => {
+    const filterValue = event.target.value.toLowerCase();
+
+    const filteredProducts = data.filter((product) =>
+      product.brand.toLowerCase().includes(filterValue)
+    );
+    setFilteredData(filteredProducts);
+  };
+
   return (
     <Container>
-      <Header
-        heading="Trending Products"
-        subHeading="Discover the Latest and Most Popular Smartphone Models"
-      />
+      <div className="flex justify-between">
+        <Header
+          heading="Trending Products"
+          subHeading="Discover the Latest and Most Popular Smartphone Models"
+        />
+        <label className="form-control w-full max-w-xs">
+          <div className="label">
+            <span className="label-text">Filter By Brand</span>
+          </div>
+          <select
+            className="select select-bordered"
+            onChange={handleFilterChange}
+          >
+            <option value="">All Brands</option>
+            <option value={"apple"}>Apple</option>
+            <option value={"samsung"}>Samsung</option>
+            <option value={"oppo"}>Oppo</option>
+            <option value={"google Pixel"}>Google Pixel</option>
+            <option value={"xiaomi"}>Xiaomi</option>
+            <option value={"onePlus"}>OnePlus</option>
+          </select>
+        </label>{" "}
+      </div>
 
-      <AllProductsGrid items={data.data} />
+      <AllProductsGrid items={filteredData} />
     </Container>
   );
 }
--- src/components/shared/Header.tsx
+++ src/components/shared/Header.tsx
@@ -7,7 +7,7 @@ type THeader = {
 
 const Header: React.FC<THeader> = ({ heading, subHeading }) => {
   return (
-    <div className="text-center ">
+    <div className="text-start ">
       <h1 className="text-5xl font-bold mb-3">{heading}</h1>
       <h3 className="text-lg text-gray-600 mb-10">{subHeading}</h3>
     </div>
